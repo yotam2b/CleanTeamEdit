7d1e07d22e4ac16e9a49de31c79b26ffea414f19
madisoncourter <148826207+madisoncourter@users.noreply.github.com>
2023-11-30 12:31:41 -0500

    Add files via upload

---
+++ CleanTeamEdit/Nudge_MessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nudge_MessagingApp.swift
+//  Nudge Messaging
+//
+//  Created by Madison  Courter on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Nudge_MessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
